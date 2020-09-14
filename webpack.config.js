f5d760c22e335209f4e1b141c432cfdacecc3dd8
David Moon <moon.kyoungje@gmail.com>
2020-09-14 15:04:58 -0400

    Add Webpack CSS loader

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,15 @@ module.exports = {
           },
         },
       },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+        ],
+      },
     ],
   },
 };
