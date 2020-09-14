0654234a269adead2a17d11dbbe9993ed23a3b72
David Moon <moon.kyoungje@gmail.com>
2020-09-14 16:02:48 -0400

    Set up Sequelize

---
+++ server/db/db.js
@@ -0,0 +1,16 @@
+const Sequelize = require("sequelize");
+
+const db = new Sequelize(
+  process.env.DATABASE_URL || "postgres://localhost:5432/boilermaker"
+);
+
+(async () => {
+  try {
+    await db.authenticate();
+    console.log("DB Authenticated");
+  } catch (error) {
+    console.error("DB Not Authenticated:", error);
+  }
+})();
+
+module.exports = db;
